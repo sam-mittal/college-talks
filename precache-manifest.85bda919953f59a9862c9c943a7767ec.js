9de0a133837caee2287217c109a673474b300f48
Sarthak Mittal <sam.mittal.2106@gmail.com>
2020-04-16 00:31:51 +0530

    Updates

---
+++ precache-manifest.85bda919953f59a9862c9c943a7767ec.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd013e7a2736018fcab3cd8110bb32e8",
+    "url": "/college-talks/index.html"
+  },
+  {
+    "revision": "b066b65b1d1aead439a3",
+    "url": "/college-talks/static/css/main.00136a3b.chunk.css"
+  },
+  {
+    "revision": "f670cc1d5e8ba719ca82",
+    "url": "/college-talks/static/js/2.ba72bed0.chunk.js"
+  },
+  {
+    "revision": "b066b65b1d1aead439a3",
+    "url": "/college-talks/static/js/main.6a6201f7.chunk.js"
+  },
+  {
+    "revision": "c44e4280c11b5466de61",
+    "url": "/college-talks/static/js/runtime-main.f250f915.js"
+  },
+  {
+    "revision": "1f94aef3843cea4f52e869c095559bf1",
+    "url": "/college-talks/static/media/Add.1f94aef3.png"
+  },
+  {
+    "revision": "cfc9818245d8159cf1503223b33c1eea",
+    "url": "/college-talks/static/media/Age.cfc98182.png"
+  },
+  {
+    "revision": "a89b3602285ccbd34754a19d6524c921",
+    "url": "/college-talks/static/media/Cross.a89b3602.png"
+  },
+  {
+    "revision": "52327c26711e767cb9f6ab02bb49e5b7",
+    "url": "/college-talks/static/media/Gender.52327c26.png"
+  },
+  {
+    "revision": "7cbf5d429877bba3ca3abcf76959889f",
+    "url": "/college-talks/static/media/Heart_Filled.7cbf5d42.png"
+  },
+  {
+    "revision": "a56d8993dbbc79615995aaef55bff3cb",
+    "url": "/college-talks/static/media/Heart_Unfilled.a56d8993.png"
+  },
+  {
+    "revision": "2edc84d4c5531664919fa90a10f28ffa",
+    "url": "/college-talks/static/media/Interest.2edc84d4.png"
+  },
+  {
+    "revision": "45c8cc35a7c07382ee8f4257ece8d8fb",
+    "url": "/college-talks/static/media/Landingpage.45c8cc35.png"
+  },
+  {
+    "revision": "0f7ddc24583501b1de6e5e652c6fb450",
+    "url": "/college-talks/static/media/Search.0f7ddc24.png"
+  },
+  {
+    "revision": "d5f6404703e4b5a193b99dfef76d67df",
+    "url": "/college-talks/static/media/Send.d5f64047.png"
+  },
+  {
+    "revision": "8ba89d3d87437c9d8c77c243c85306b4",
+    "url": "/college-talks/static/media/Username.8ba89d3d.png"
+  },
+  {
+    "revision": "64af3f5d6f2bbb011a5cbb3f51f0ca5c",
+    "url": "/college-talks/static/media/dating.64af3f5d.jpeg"
+  },
+  {
+    "revision": "b63e146c31c62bdad9315ecf216969c7",
+    "url": "/college-talks/static/media/dropdown-arrow.b63e146c.png"
+  },
+  {
+    "revision": "c19a30e30d9f1cdde7acd197acacb65d",
+    "url": "/college-talks/static/media/feed.c19a30e3.jpeg"
+  },
+  {
+    "revision": "b94333a3b7d4e09e360629fc458b22d0",
+    "url": "/college-talks/static/media/marketplace.b94333a3.jpeg"
+  },
+  {
+    "revision": "ec4a3d84d9d5820b4ae4d1cafa384e95",
+    "url": "/college-talks/static/media/notes.ec4a3d84.jpeg"
+  }
+]);
